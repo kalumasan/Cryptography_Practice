de58f39fcc20240698a7e2c75d7e9ff8269508ee
cwycccc <140133287+cwycccc@users.noreply.github.com>
2023-07-27 23:06:41 +0800

    Update cert.py
    使用sha512

--- example/cert.py
+++ example/cert.py
@@ -11,7 +11,7 @@ certificateFile='./cert/selfsignedCertificate.pem'
 def generatePrivateKey():
     keyString=rsa.generate_private_key(
         public_exponent=65537,
-        key_size=1024,
+        key_size=4096,
     )
     with open(privateKeyFile,'wb')as f:
         f.write(
@@ -34,7 +34,7 @@ def generateCertificate():
         x509.NameAttribute(NameOID.COMMON_NAME, u'www.cuccloud.getuplate.com'),
         x509.NameAttribute(ExtensionOID.SUBJECT_ALTERNATIVE_NAME, u'DNS:www.cuccloud.getuplate.com'),
     ])
-    certString=x509.CertificateBuilder().subject_name(subjectInfo).issuer_name(issuer).public_key(keyString.public_key()).serial_number(x509.random_serial_number()).not_valid_before(datetime.utcnow()).not_valid_after(datetime.utcnow()+timedelta(days=10)).add_extension(x509.SubjectAlternativeName([x509.DNSName(u'www.cuccloud.getuplate.com')]), critical=False).sign(keyString, hashes.SHA256())
+    certString=x509.CertificateBuilder().subject_name(subjectInfo).issuer_name(issuer).public_key(keyString.public_key()).serial_number(x509.random_serial_number()).not_valid_before(datetime.utcnow()).not_valid_after(datetime.utcnow()+timedelta(days=10)).add_extension(x509.SubjectAlternativeName([x509.DNSName(u'www.cuccloud.getuplate.com')]), critical=False).sign(keyString, hashes.SHA512())
     with open(certificateFile,'wb')as f:
         f.write(certString.public_bytes(serialization.Encoding.PEM))
     return certString
